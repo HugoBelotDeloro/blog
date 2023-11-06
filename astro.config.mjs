9b672135e7a077d2ab2ccaea08de365fdc33153b
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-11-06 20:55:40 +0100

    posts: add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/lib/remark-plugins/reading-time";
 
 export default defineConfig({
   site: "https://hugobelotdeloro.github.io/",
   integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
