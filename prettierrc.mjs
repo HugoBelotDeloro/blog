51c20a3514da09fbd594123618214ef8695f9b72
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-11-10 09:55:13 +0100

    format: configure prettier-plugin-astro

---
+++ prettierrc.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  astroAllowShorthand: true,
+};
+
+export default config;
