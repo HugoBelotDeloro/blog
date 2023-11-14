9609d785331f412fc2720dfe562daa33dc787aa6
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-11-14 11:26:52 +0100

    posts: cache post rendering

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,10 +1,30 @@
-import { getCollection } from "astro:content";
+import {
+  getCollection,
+  getEntry,
+  type CollectionEntry,
+  type Render,
+} from "astro:content";
 
 export async function getAllPostsSorted() {
-  const posts = await getCollection(
+  const posts: CollectionEntry<"posts">[] = await getCollection(
     "posts",
     (post) => post.data.draft !== true,
   );
 
   return posts.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+}
+
+const slugsToRenderedPosts: Record<string, Awaited<Render[".mdx"]>> = {};
+
+export async function getRenderedPost(slug: string) {
+  const post = await getEntry("posts", slug);
+  if (post === undefined) {
+    throw new Error(`Unknown post slug "${slug}"`);
+  }
+  let rendered = slugsToRenderedPosts[slug];
+  if (rendered === undefined) {
+    rendered = await post.render();
+    slugsToRenderedPosts[slug] = rendered;
+  }
+  return rendered;
 }
