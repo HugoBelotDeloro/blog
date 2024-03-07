d3484c9dd3377a2081ab54afb1cb82f3b2734c79
Hugo Belot-Deloro <hugo.belot-deloro@proton.me>
2024-03-07 18:19:57 +0100

    nav: centralise links

---
+++ src/lib/links.ts
@@ -0,0 +1,8 @@
+export const links = (() => {
+  const github = "https://github.com/HugoBelotDeloro";
+  const repo = "blog";
+  const issues = `${github}/${repo}/issues`;
+  const linkedin = "https://www.linkedin.com/in/hugo-belot-deloro/";
+
+  return { github, repo, issues, linkedin };
+})();
